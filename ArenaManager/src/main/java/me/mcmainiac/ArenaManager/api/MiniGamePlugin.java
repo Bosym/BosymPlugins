5d723e68e241dd63cdd945202d7370619768039c
Ricardo Boss <ricardo.boss@web.de>
2016-08-27 09:52:00 +0200

    Updated

---
+++ ArenaManager/src/main/java/me/mcmainiac/ArenaManager/api/MiniGamePlugin.java
@@ -0,0 +1,38 @@
+package me.mcmainiac.ArenaManager.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+public abstract class MiniGamePlugin extends JavaPlugin {
+    protected static Logger log = Logger.getLogger("Minecraft");
+    protected static String name = null;
+
+    static {
+        log.setUseParentHandlers(false);
+        log.addHandler(new Handler() {
+            private SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+
+            @Override
+            public void publish(LogRecord record) {
+                StringBuilder sb = new StringBuilder();
+
+                sb.append('[').append(sdf.format(new Date())).append(' ');
+                sb.append(record.getLevel().toString().toUpperCase()).append("]: ");
+                sb.append(record.getMessage());
+
+                System.out.println(sb.toString());
+            }
+
+            @Override
+            public void flush() {}
+
+            @Override
+            public void close() throws SecurityException {}
+        });
+    }
+}
---
+++ KingUndercover/src/main/java/me/mcmainiac/KingUndercover/Main.java
@@ -0,0 +1,12 @@
+package me.mcmainiac.KingUndercover;
+
+import me.mcmainiac.ArenaManager.api.MiniGamePlugin;
+
+public class Main extends MiniGamePlugin {
+    @Override
+    public void onEnable() {
+        name = "KingUndercover";
+
+        log.info("Enabled");
+    }
+}
